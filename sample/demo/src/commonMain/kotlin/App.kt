f72e6b549eb68131ede9200755415c3e8d1a7fe9
Yuya <140081005+oikvpqya@users.noreply.github.com>
2024-07-10 13:07:37 +0900

    feat: Minor change demo app

--- sample/demo/src/commonMain/kotlin/App.kt
+++ sample/demo/src/commonMain/kotlin/App.kt
@@ -1,15 +1,26 @@
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
     AppTheme {
         Surface {
-            Screen()
+            Scaffold(
+                topBar = { TopAppBar(title = { Text("FastScroller for Compose Multiplatform Demo") } ) }
+            ) { innerPadding ->
+                Screen(Modifier.padding(innerPadding))
+            }
         }
     }
 }
